4bf106c3383ff8512b226e99e0e0cce722b55b27
Natalia Braz <naabraz@gmail.com>
2020-02-05 19:50:01 -0300

    chore(graphql): adjusting structure

--- server.ts
+++ server.ts
@@ -2,18 +2,14 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import cors from 'cors';
-import schema from './src/schema';
+import { schema } from './src/schema';
 
 const app = express();
-const server = new ApolloServer({
-  schema
-});
+const server = new ApolloServer({ schema });
 
 app.use('*', cors());
 
 server.applyMiddleware({ app, path: '/graphql' });
 
 const httpServer = createServer(app);
-httpServer.listen(
-  { port: 3000 },
-  (): void => console.log(`🚀 GraphQL is now running on http://localhost:3000/graphql`));
+httpServer.listen({ port: 3000 }, (): void => console.log(`🚀 GraphQL is running on http://localhost:3000/graphql`));
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -2,12 +2,10 @@ import 'graphql-import-node';
 import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 
-import { types } from './types';
-import { resolvers } from './resolvers';
+import { types } from '../types';
+import { resolvers } from '../resolvers';
 
-const schema: GraphQLSchema = makeExecutableSchema({
+export const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs: types,
   resolvers,
 });
-
-export default schema;
