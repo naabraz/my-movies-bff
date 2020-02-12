e75936ff1c0bec58692d515d43276032313024cd
Natalia Braz <naabraz@gmail.com>
2020-02-12 19:32:42 -0300

    chore(popularmovies): improving returned values

--- src/data/PopularMovies/GetPopularMovies.ts
+++ src/data/PopularMovies/GetPopularMovies.ts
@@ -2,6 +2,18 @@ import fetch from 'node-fetch';
 
 import { PopularMovies } from './IPopularMovies';
 
+const handlePopularMovies = ({ results }: { results: any }): PopularMovies[] => {
+  const popularMovies = results.map((movie: any) => ({
+    id: movie.id,
+    posterPath: movie.posterPath,
+    title: movie.title,
+    releaseDate: movie.releaseDate,
+    overview: movie.overview,
+  }));
+
+  return popularMovies;
+}
+
 export const getPopularMovies = async (): Promise<PopularMovies[]> => {
   const API_URL = process.env.API_URL
   const API_KEY = process.env.API_KEY
@@ -9,13 +21,5 @@ export const getPopularMovies = async (): Promise<PopularMovies[]> => {
   const response = await fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`);
   const jsonResponse = await response.json();
 
-  const popularMovies = jsonResponse.results.map((movie: any ) => ({
-    id: movie.id,
-    posterPath: movie.poster_path,
-    title: movie.title,
-    releaseDate: movie.release_date,
-    overview: movie.overview
-  }));
-
-  return popularMovies;
+  return handlePopularMovies(jsonResponse);
 };
