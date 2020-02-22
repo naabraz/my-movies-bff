25976777a594813da43b9adf74bbe496c3027788
Natalia Braz <naabraz@gmail.com>
2020-02-22 18:01:45 -0300

    chore(unit test): adding unit test

---
+++ src/resolvers/PopularMovies.test.ts
@@ -0,0 +1,28 @@
+import { getPopularMovies } from '../data';
+import { resolvers } from './';
+import { PopularMoviesResolver } from './PopularMovies';
+
+jest.mock('../data', () => ({
+  getPopularMovies: jest.fn().mockResolvedValue('getPopularMovies'),
+}));
+
+describe('Given PopularMovies resolver', () => {
+  it('Should have an array of resolvers', () => {
+    expect(resolvers).toEqual(expect.any(Array));
+  });
+
+  it('Should have Query property', () => {
+    expect(PopularMoviesResolver).toHaveProperty('Query');
+  });
+
+  it('Should have popularMovies query', () => {
+    expect(PopularMoviesResolver.Query).toHaveProperty('popularMovies');
+  });
+
+  describe('Given popularMovies query', () => {
+    it('Should call getPopularMovies when popularMovies query is called', async () => {
+      await PopularMoviesResolver.Query.popularMovies();
+      expect(getPopularMovies).toHaveBeenCalled();
+    });
+  });
+});
