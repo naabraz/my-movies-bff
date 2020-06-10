e6472505a5cd755d5287f2f06d0ef966073f7bfb
Natalia Braz <naabraz@gmail.com>
2020-06-09 22:55:10 -0300

    chore(movie details): adding unit test

---
+++ src/resolvers/MovieGenres.test.ts
@@ -0,0 +1,32 @@
+import { getMovieGenres } from '../data';
+import { resolvers } from './';
+import { MovieGenresResolver } from './MovieGenres';
+
+jest.mock('../data', () => ({
+  getMovieGenres: jest.fn().mockResolvedValue('getMovieGenres'),
+}));
+
+describe('Given MovieGenres resolver', () => {
+  it('Should have an array of resolvers', () => {
+    expect(resolvers).toEqual(expect.any(Array));
+  });
+
+  it('Should have Query property', () => {
+    expect(MovieGenresResolver).toHaveProperty('Query');
+  });
+
+  it('Should have movieGenres query', () => {
+    expect(MovieGenresResolver.Query).toHaveProperty('movieGenres');
+  });
+
+  describe('Given movieGenres query', () => {
+    it('Should call getMovieGenres when movieGenres query is called', async () => {
+      await MovieGenresResolver.Query.movieGenres(
+        { movieId: 3 },
+        { movieId: 3 },
+        { movieId: 3 },
+      );
+      expect(getMovieGenres).toHaveBeenCalledWith(3);
+    });
+  });
+});
