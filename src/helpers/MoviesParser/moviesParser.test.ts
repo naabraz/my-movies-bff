61fc084cb20084edd82535f1abf5bc8d62b0409d
Natalia Braz <naabraz@gmail.com>
2020-06-23 19:59:48 -0300

    chore(movieParser): adding unit test

---
+++ src/helpers/MoviesParser/moviesParser.test.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { moviesParser } from '~helpers';
+
+jest.mock('~helpers/imageConfig', () => ({
+  imageConfig: jest.fn().mockResolvedValue({
+    baseUrl: 'baseUrl',
+    posterSize: '300',
+    backdropSize: '300',
+  }),
+}));
+
+describe('Given MoviesParser', () => {
+  it('Should return correct object with movies', async () => {
+    const movieResponseMock = {
+      results: [
+        {
+          id: 1,
+          poster_path: 'posterPath',
+          title: 'title',
+          release_date: '2020-02-21',
+          overview: 'overview',
+          backdrop_path: 'backDropPath',
+          vote_average: 10,
+        },
+      ],
+    };
+
+    const moviesParsedExpected = [
+      {
+        id: 1,
+        posterPath: 'baseUrl300posterPath',
+        title: 'title',
+        releaseDate: '2020-02-21',
+        overview: 'overview',
+        backdropPath: 'baseUrl300backDropPath',
+        voteAverage: 10,
+      },
+    ];
+
+    const moviesParsed = await moviesParser(movieResponseMock);
+
+    expect(moviesParsed).toEqual(moviesParsedExpected);
+  });
+});
