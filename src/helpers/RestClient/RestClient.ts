178d290a282f9ef9148bb211e2fd91350a468b86
Natalia Braz <naabraz@gmail.com>
2020-06-23 20:44:07 -0300

    chore(movies by genre): adjusting request client to handle url params

--- src/helpers/RestClient/RestClient.ts
+++ src/helpers/RestClient/RestClient.ts
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const request = async (endpoint: string): Promise<any> => {
+export const request = async (endpoint: string, params = ''): Promise<any> => {
   const { API_URL, API_KEY } = process.env;
+  const url = `${API_URL}/${endpoint}?api_key=${API_KEY}${params}`;
 
-  return (await fetch(`${API_URL}/${endpoint}?api_key=${API_KEY}`)).json();
+  return (await fetch(url)).json();
 };
