0a00a84fb069b599395f3e97269f1f8c0f674719
Natalia Braz <naabraz@gmail.com>
2020-06-23 23:32:08 -0300

    chore(jest config): removing module wrapper

--- jest.config.js
+++ jest.config.js
@@ -11,12 +11,4 @@ module.exports = {
       statements: 90,
     },
   },
-  moduleNameMapper: {
-    '^~helpers(.*)$': '<rootDir>/src/helpers$1',
-    '^~data(.*)$': '<rootDir>/src/data$1',
-    '^~interfaces(.*)$': '<rootDir>/src/interfaces$1',
-    '^~resolvers(.*)$': '<rootDir>/src/resolvers$1',
-    '^~schema(.*)$': '<rootDir>/src/schema$1',
-    '^~types(.*)$': '<rootDir>/src/types$1',
-  },
 };
