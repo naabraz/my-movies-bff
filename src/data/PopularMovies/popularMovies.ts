7da5975b7a39693c72c4c0314e140acad0f9e196
Natalia Braz <naabraz@gmail.com>
2020-06-30 20:40:37 -0300

    Delete popularMovies.ts

--- src/data/PopularMovies/popularMovies.ts
+++
@@ -1,8 +0,0 @@
-import { moviesParser, request } from '~helpers';
-import { Movie } from '~interfaces';
-
-export const popularMovies = async (): Promise<Movie[]> => {
-  const popularMovies = await request('movie/popular');
-
-  return moviesParser(popularMovies);
-};
