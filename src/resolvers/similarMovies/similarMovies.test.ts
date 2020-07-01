5360c57713e468e29e31f219ffb54764c095686a
Natalia Braz <naabraz@gmail.com>
2020-06-30 22:17:59 -0300

    chore(similar movies): add resolver unit test

---
+++ src/resolvers/similarMovies/similarMovies.test.ts
@@ -0,0 +1,36 @@
+import { similarMovies } from '~data';
+import { resolvers } from '~resolvers';
+import { similarMoviesResolver } from './similarMovies';
+
+jest.mock('~data', () => ({
+  similarMovies: jest.fn().mockResolvedValue('similarMovies'),
+}));
+
+describe('Given similarMovies resolver', () => {
+  it('Should have an array of resolvers', () => {
+    expect(resolvers).toEqual(expect.any(Array));
+  });
+
+  it('Should have Query property', () => {
+    expect(similarMoviesResolver).toHaveProperty('Query');
+  });
+
+  it('Should have similarMovies query', () => {
+    expect(similarMoviesResolver.Query).toHaveProperty('similarMovies');
+  });
+
+  it('Should have similarMovies query', () => {
+    expect(similarMoviesResolver.Query).toHaveProperty('similarMovies');
+  });
+
+  describe('Given moviesByGenre query', () => {
+    it('Should call similarMovies data when similarMovies query is called', async () => {
+      await similarMoviesResolver.Query.similarMovies(
+        { movieId: 3 },
+        { movieId: 3 },
+        { movieId: 3 },
+      );
+      expect(similarMovies).toHaveBeenCalledWith(3);
+    });
+  });
+});
