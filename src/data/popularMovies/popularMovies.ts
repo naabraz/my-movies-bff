eb75578efa9660c33b5f4d6da7b275ad1601c18a
Natalia Braz <naabraz@gmail.com>
2020-06-30 21:08:36 -0300

    chore(popular movies): adjust var name

--- src/data/popularMovies/popularMovies.ts
+++ src/data/popularMovies/popularMovies.ts
@@ -2,7 +2,7 @@ import { moviesParser, request } from '~helpers';
 import { Movie } from '~interfaces';
 
 export const popularMovies = async (): Promise<Movie[]> => {
-  const popularMovies = await request('movie/popular');
+  const movies = await request('movie/popular');
 
-  return moviesParser(popularMovies);
+  return moviesParser(movies);
 };
