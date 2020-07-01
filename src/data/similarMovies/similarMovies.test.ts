61bd0738aa0e37314f6a3fb5ef26451bf51dfd13
Natalia Braz <naabraz@gmail.com>
2020-06-30 22:08:45 -0300

    chore(similar movies): add data unit test

---
+++ src/data/similarMovies/similarMovies.test.ts
@@ -0,0 +1,25 @@
+import { similarMovies } from '~data';
+import { request, similarMoviesParser } from '~helpers';
+
+jest.mock('~helpers', () => ({
+  request: jest.fn().mockImplementation(() => ({ results: [] })),
+  imageConfig: jest.fn(),
+  similarMoviesParser: jest.fn(),
+}));
+
+describe('Given similarMovies data', () => {
+  it('Should have similarMovies function', () => {
+    expect(similarMovies).toEqual(expect.any(Function));
+  });
+
+  describe('Given similarMovies function call', () => {
+    it('Should call request', async () => {
+      await similarMovies(3);
+      expect(request).toHaveBeenCalled();
+    });
+
+    it('Should return parsed similar movies', async () => {
+      expect(similarMoviesParser).toHaveBeenCalled();
+    });
+  });
+});
