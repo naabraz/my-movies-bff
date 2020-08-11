d0b856ea76450124bb40b1b4a246ecde625804ec
Natalia Braz <naabraz@gmail.com>
2020-08-11 20:56:39 -0300

    chore(travis): add stage to deploy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  cd public
+  git init
+
+  git remote add deploy "travis@nataliabraz.dev:/home/naabraz/app"
+  git config user.name "Travis CI"
+  git config user.email "naabraz@gmail.com"
+
+  git add .
+  git commit -m "Deploy"
+  git push --force deploy master
+else
+  echo "Not deploying, since this branch isn't master."
+fi
