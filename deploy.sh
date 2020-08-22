b2dad31619e489b38660719f6c5195ea8a8a622c
Natalia Braz <naabraz@gmail.com>
2020-08-22 15:14:52 -0300

    chore(travis): adjust script

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  cd opt
-  cd website
-  cd public
-  git init
-
-  git remote add deploy "travis@nataliabraz.dev:/home/naabraz/app"
-  git config user.name "Travis CI"
-  git config user.email "naabraz@gmail.com"
-
-  git add .
-  git commit -m "Deploy"
-  git push --force deploy master
+  tar -czf package.tgz .
+  scp package.tgz travis@nataliabraz.dev:~/build
 else
   echo "Not deploying, since this branch isn't master."
 fi
