7bd8235491788275abd6cd3b3bc955c2622c6d28
Natalia Braz <naabraz@gmail.com>
2020-08-22 15:55:11 -0300

    chore(deploy): add untar script

--- send.sh
+++ send.sh
@@ -2,5 +2,6 @@
 set -xe
 
 scp package.tgz travis@nataliabraz.dev:~/build
+ssh travis@nataliabraz.dev 'bash -s' < ./untar.sh
 
 echo "package sent"
---
+++ untar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -xe
+
+# export NODE_ENV=production
+# export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
+
+cd /build && tar zxvf package.tgz
