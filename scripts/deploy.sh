041ee5f24164a8aa0a26e0391f2034fd2e7b5451
Natalia Braz <naabraz@gmail.com>
2020-08-23 17:41:27 -0300

    chore(travis); adjust script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 set -x
 
-cd .. && \
-rm -rf node_modules && \
 cd $HOME/build/naabraz && \
 tar -czf package.tgz my-movies-bff && \
 scp package.tgz travis@nataliabraz.dev:~ && \
