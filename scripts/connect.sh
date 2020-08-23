005f2386d208ff28819af72fef67424e6cdf2e49
Natalia Braz <naabraz@gmail.com>
2020-08-23 15:19:25 -0300

    chore(travis): adjust script

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-set -xe
+#!/usr/bin/env sh
+set -x
 
+tar -czf package.tgz . && \
+scp package.tgz travis@nataliabraz.dev:~/build && \
 ssh travis@nataliabraz.dev 'bash -s' < ./untar.sh
