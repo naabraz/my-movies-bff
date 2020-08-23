8374f3cb4912d5b50af82e87ec0a54aee65a6509
Natalia Braz <naabraz@gmail.com>
2020-08-23 17:20:18 -0300

    chore(deploy): removing node modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -x
 
+cd .. && \
+rm -rf node_modules && \
 cd $HOME/build/naabraz && \
 tar -czf package.tgz my-movies-bff && \
 scp package.tgz travis@nataliabraz.dev:~ && \
