48b38f8f59c3ba1122e3c1622d984985918231ea
Natalia Braz <naabraz@gmail.com>
2021-05-10 21:51:48 -0300

    chore: remove unused folders

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -x
-
-cd $HOME/build/naabraz && \
-tar -czf package.tgz my-movies-bff && \
-scp package.tgz travis@nataliabraz.dev:~ && \
-cd $TRAVIS_BUILD_DIR && \
-ssh travis@nataliabraz.dev 'bash -s' < ./scripts/untar.sh
